1676ecbab2f6f271003dd50b341af357b9769209
Lahmizzar Valeryon <lahmizzar@devxive.com>
2015-10-08 00:14:04 +0200

    initial

---
+++ usr/share/univention-wsccubuntu-integration/ucs-ldap-integration.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+# echo -e "\e[38;5;87mXXX\e[0m"
+
+echo "##########################################################"
+echo "#                                                        #"
+echo -e "#  \e[38;5;87mWelcome to the WALZ.systems Corporate Client Patcher\e[0m  #"
+echo "# ------------------------------------------------------ #"
+echo "#                                                        #"
+echo -e "# \e[38;5;87mNow we'll install all UCS ldap dependencies for Ubuntu\e[0m #"
+echo "#                                                        #"
+echo -e "# \e[38;5;87mAfter that, you're able to login with your UCS login\e[0m   #"
+echo -e "# \e[38;5;87mcredentials. BUT PLEASE WAIT FOR THE SYSTEM REBOOT!\e[0m    #"
+echo "#                                                        #"
+echo "##########################################################"
+echo ""
+echo ""
+read -p "Enter the ip address from the UCS DC Master: " ipucsmaster
+read -p "Are you sure that your ip address is $ipucsmaster? (Yes/No): " ipucsmaster_answer
+
+if [[ $ipucsmaster_answer == "No" || $ipucsmaster_answer == "N" || $ipucsmaster_answer == "n" || $ipucsmaster_answer == "" ]]; then
+	echo -e "\e[38;5;196mIf you want to resume the installation, you have to enter the following\e[0m"
+	echo -e "\e[38;5;202msudo bash /usr/share/univention-wsccubuntu-integration/ucs-ldap-integration.sh\e[0m"
+	exit 1;
+fi
+
+# Set the IP address of the UCS DC Master, 192.168.0.3 in this example
+export MASTER_IP=$ipucsmaster
+
+mkdir /etc/univention
+ssh root@${MASTER_IP} ucr shell |
+ grep -v ^hostname= >/etc/univention/ucr_master
+echo "master_ip=${MASTER_IP}" >>/etc/univention/ucr_master
+chmod 660 /etc/univention/ucr_master
+. /etc/univention/ucr_master
+
+echo "${MASTER_IP} ${ldap_master}" >>/etc/hosts
+
+
+
+# Set some environment variables
+. /etc/univention/ucr_master
+
+# Download the SSL certificate
+mkdir -p /etc/univention/ssl/ucsCA/
+wget -O /etc/univention/ssl/ucsCA/CAcert.pem \
+    http://${ldap_master}/ucs-root-ca.crt
+
+# Create an account and save the password
+password="$(tr -dc A-Za-z0-9_ </dev/urandom | head -c20)"
+ssh root@${ldap_master} udm computers/ubuntu create \
+    --position "cn=computers,${ldap_base}" \
+    --set name=$(hostname) --set password="${password}" \
+    --set operatingSystem="$(lsb_release -is)" \
+    --set operatingSystemVersion="$(lsb_release -rs)"
+printf '%s' "$password" >/etc/ldap.secret
+chmod 0400 /etc/ldap.secret
+
+# Create ldap.conf
+cat >/etc/ldap/ldap.conf <<__EOF__
+TLS_CACERT /etc/univention/ssl/ucsCA/CAcert.pem
+URI ldap://$ldap_master:7389
+BASE $ldap_base
+__EOF__
+
+
+
+# Set some environment variables
+. /etc/univention/ucr_master
+
+# Install SSSD based configuration
+DEBIAN_FRONTEND=noninteractive apt-get -y install sssd libnss-sss libpam-sss libsss-sudo
+
+# Create sssd.conf
+cat >/etc/sssd/sssd.conf <<__EOF__
+[sssd]
+config_file_version = 2
+reconnection_retries = 3
+sbus_timeout = 30
+services = nss, pam, sudo
+domains = $kerberos_realm
+
+[nss]
+reconnection_retries = 3
+
+[pam]
+reconnection_retries = 3
+
+[domain/$kerberos_realm]
+auth_provider = krb5
+krb5_kdcip = ${master_ip}
+krb5_realm = ${kerberos_realm}
+krb5_server = ${ldap_master}
+krb5_kpasswd = ${ldap_master}
+id_provider = ldap
+ldap_uri = ldap://${ldap_master}:7389
+ldap_search_base = ${ldap_base}
+ldap_tls_reqcert = never
+ldap_tls_cacert = /etc/univention/ssl/ucsCA/CAcert.pem
+cache_credentials = true
+enumerate = true
+ldap_default_bind_dn = cn=$(hostname),cn=computers,${ldap_base}
+ldap_default_authtok_type = password
+ldap_default_authtok = $(cat /etc/ldap.secret)
+__EOF__
+chmod 600 /etc/sssd/sssd.conf
+
+# Install auth-client-config
+DEBIAN_FRONTEND=noninteractive apt-get -y install auth-client-config
+
+# Create an auth config profile for sssd
+cat >/etc/auth-client-config/profile.d/sss <<__EOF__
+[sss]
+nss_passwd=   passwd:   compat sss
+nss_group=    group:    compat sss
+nss_shadow=   shadow:   compat
+nss_netgroup= netgroup: nis
+
+pam_auth=
+        auth [success=3 default=ignore] pam_unix.so nullok_secure try_first_pass
+        auth requisite pam_succeed_if.so uid >= 500 quiet
+        auth [success=1 default=ignore] pam_sss.so use_first_pass
+        auth requisite pam_deny.so
+        auth required pam_permit.so
+
+pam_account=
+        account required pam_unix.so
+        account sufficient pam_localuser.so
+        account sufficient pam_succeed_if.so uid < 500 quiet
+        account [default=bad success=ok user_unknown=ignore] pam_sss.so
+        account required pam_permit.so
+
+pam_password=
+        password sufficient pam_unix.so obscure sha512
+        password sufficient pam_sss.so use_authtok
+        password required pam_deny.so
+
+pam_session=
+        session required pam_mkhomedir.so skel=/etc/skel/ umask=0077
+        session optional pam_keyinit.so revoke
+        session required pam_limits.so
+        session [success=1 default=ignore] pam_sss.so
+        session required pam_unix.so
+__EOF__
+auth-client-config -a -p sss
+
+# Restart sssd
+restart sssd
+
+
+
+cat >/usr/share/pam-configs/ucs_mkhomedir <<__EOF__
+Name: activate mkhomedir
+Default: yes
+Priority: 900
+Session-Type: Additional
+Session:
+    required    pam_mkhomedir.so umask=0022 skel=/etc/skel
+__EOF__
+
+DEBIAN_FRONTEND=noninteractive pam-auth-update --force
+
+
+
+echo '*;*;*;Al0000-2400;audio,cdrom,dialout,floppy,plugdev,adm' \
+   >>/etc/security/group.conf
+
+cat >>/usr/share/pam-configs/local_groups <<__EOF__
+Name: activate /etc/security/group.conf
+Default: yes
+Priority: 900
+Auth-Type: Primary
+Auth:
+    required    pam_group.so use_first_pass
+__EOF__
+
+DEBIAN_FRONTEND=noninteractive pam-auth-update
+
+
+
+# Add a field for a user name, disable user selection at the login screen
+mkdir /etc/lightdm/lightdm.conf.d
+cat >>/etc/lightdm/lightdm.conf.d/99-show-manual-userlogin.conf <<__EOF__
+[SeatDefaults]
+greeter-show-manual-login=true
+greeter-hide-users=true
+__EOF__
+
+
+
+echo -e "\e[38;5;87m...successfully LDAP integrategration in UCS DC Master\e[0m"
--- usr/share/univention-wsccubuntu-integration/wscc-patcher.sh
+++ usr/share/univention-wsccubuntu-integration/wscc-patcher.sh
@@ -2,7 +2,7 @@
 
 echo "##########################################################"
 echo "#                                                        #"
-echo -e "#  \e[96mWelcome to the WALZ.systems Corporate Client Patcher\033[0m  #"
+echo -e "#  \e[38;5;87mWelcome to the WALZ.systems Corporate Client Patcher\e[0m  #"
 echo "# ------------------------------------------------------ #"
 echo "#                                                        #"
 echo "# This will install the WSCC - OS Patch on a clean       #"
@@ -15,65 +15,149 @@ echo "#                                                        #"
 echo "##########################################################"
 echo ""
 echo ""
-echo "We'll now reactivate the root account. Please follow the instructions below"
-echo "First, you have to enter the password for the sysmaster account"
+echo "We'll now reactivate the root account. Please follow the instructions below."
+echo "First, you have to enter the password for the sysmaster account!"
 echo ">> After that, you have to set a password for the root account:"
-sudo passwd root
-sudo cp /usr/share/univention-wsccubuntu-integration/files/sshd_config /etc/ssh/sshd_config
-sudo service ssh restart
+passwd root
 
-exit 1;
+## Check if backup of original exists, else create a copy
+echo ""
+echo ""
+if [ ! -f /etc/ssh/sshd_config.ORIG ]; then
+	echo -e "\e[38;5;46mMove /etc/ssh/sshd_config original file to /etc/ssh/sshd_config.ORIG\e[0m"
+	cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
+else
+	echo -e "\e[38;5;202mSkip backup, because the original file already exists\e[0m"
+fi
+cp /usr/share/univention-wsccubuntu-integration/files/sshd_config /etc/ssh/sshd_config
+echo ""
+echo ""
+echo -e "\e[38;5;227m...restart ssh service\e[0m"
+service ssh restart
+
+echo ""
+echo ""
+echo -e "\e[38;5;227mPerforming complete system updates. This may take some time!\e[0m"
+echo -e "\e[38;5;227m(based on your internet connection)\e[0m"
 # ------------------------------------------------------ INSTALL UPDATE --------------------------------------------------------
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y
 
-exit 1;
 
 # --------------------------------------------------- INSTALL APPLICATIONS -----------------------------------------------------
 # Install numlockx and enable it for lightdm (on login screen)
-apt-get install numlockx
+echo -e "\e[38;5;112mInstall and activate numlockx (used to activate num lock key on lightdm login screen)\e[0m"
+apt-get install numlockx -y
 echo "greeter-setup-script=/usr/bin/numlockx on" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 
 
 # Install NFS-Commons
-apt-get install nfs-common
+echo -e "\e[38;5;112mInstall nfs-common (need to mount nfs directories)\e[0m"
+apt-get install nfs-common -y
+
+
+# Install VIM-Editor
+echo -e "\e[38;5;112mInstall vim (a nice and colored texteditor)\e[0m"
+apt-get install vim -y
 
 
 # Install OwnCloud Client
+echo -e "\e[38;5;112mSet owncloud repository and Install owncloud-client (owncloud linux client)\e[0m"
 wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_14.04/Release.key
-sudo apt-key add - < Release.key
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
-sudo apt-get update
-sudo apt-get install owncloud-client
+apt-key add - < Release.key
+mkdir -p /usr/share/univention-wsccubuntu-integration/commons/owncloud
+mv Release.key /usr/share/univention-wsccubuntu-integration/commons/owncloud
+sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+apt-get update
+apt-get install owncloud-client -y
+
 
 
 # -------------------------------------------------- UNINSTALL APPLICATIONS ----------------------------------------------------
 # Einzelanwendungen
-#	brasero			= CD/DVD Brennprogramm
-#	cheese			= Webcam-Automat
-#	xul-ext-ubufox		= Ubuntu Änderungserweiterung für Firefox
-#	unity-scope-gdrive	= Google Drive Integration
-sudo apt-get purge brasero cheese xul-ext-ubufox unity-scope-gdrive
+#	brasero			= CD/DVD Brennprogramm				# Auto Installed Packages (AIP): brasero-cdrkit
+#	cheese			= Webcam-Automat				# AIP: gnome-video-effects
+#	xul-ext-ubufox		= Ubuntu Änderungserweiterung für Firefox	# AIP: N/A
+#	unity-scope-gdrive	= Google Drive Integration			# AIP: N/A
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall brasero\e[0m"
+apt-get purge -y brasero
+
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall cheese\e[0m"
+apt-get purge -y cheese
+
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall xul-ext-ubufox\e[0m"
+apt-get purge -y xul-ext-ubufox
+
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall unity-scope-gdrive\e[0m"
+apt-get purge -y unity-scope-gdrive
+
 
 # Spiele
-sudo apt-get purge aisleriot gnome-mahjongg gnome-sudoku gnome-mines gnomine
+# 	aisleriot								# AIP: guile-2.0-libs libgc1c2
+#	gnome-mahjongg								# AIP: N/A
+#	gnome-sudoku								# AIP: N/A
+#	gnome-mines								# AIP: N/A
+#	gnomine									# AIP: N/A				// wird mit gnome-mines deinstalliert
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall various games\e[0m"
+apt-get purge -y aisleriot gnome-mahjongg gnome-sudoku gnome-mines gnomine
+
 
 # Programm zur Videowiedergabe
-sudo apt-get purge totem totem-mozilla totem-plugins
+# 	totem									# AIP: gir1.2-totem-1.0 gir1.2-totem-plparser-1.0 libtotem0 totem-common
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall totem and its extensions\e[0m"
+apt-get purge -y totem totem-mozilla totem-plugins
+
 
 # Programm zur Fotoverwaltung und -betrachtung inkl. Erweiterungen
-sudo apt-get purge shotwell shotwell-common
+# 	shotwell								# AIP: libexiv2-12 libgexiv2-2 libraw9
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall shotwell and its extensions\e[0m"
+apt-get purge -y shotwell shotwell-common
+
 
 # eMail CLient inkl. Erweiterungen
-sudo apt-get purge thunderbird thunderbird-gnome-support thunderbird-locale-en thunderbird-locale-de thunderbird-locale-en-us
+# 	thunderbird								# AIP: N/A
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall thunderbird and its extensions\e[0m"
+apt-get purge -y thunderbird thunderbird-gnome-support thunderbird-locale-en thunderbird-locale-de thunderbird-locale-en-us
+
 
 # Programm zur Audiowiedergabe inkl. Erweiterungen
-sudo apt-get purge rhythmbox rhythmbox-plugin-zeitgeist rhythmbox-plugin-cdrecorder rhythmbox-mozilla rhythmbox-plugin-magnatune rhythmbox-plugins
+# 	rhythmbox								# AIP: brasero-common dvd+rw-tools gir1.2-gnomekeyring-1.0 gir1.2-rb-3.0 gir1.2-secret-1 growisofs libbrasero-media3-1 libburn4 libdmapsharing-3.0-2 libgmime-2.6-0 libgpod-common libgpod4 libisofs6 libjte1 liblircclient0 librhythmbox-core8 libsgutils2-2 libtotem-plparser18 media-player-info python3-mako python3-markupsafe rhythmbox-data
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall rhythmbox and its extensions\e[0m"
+apt-get purge -y rhythmbox rhythmbox-plugin-zeitgeist rhythmbox-plugin-cdrecorder rhythmbox-mozilla rhythmbox-plugin-magnatune rhythmbox-plugins
+
+
 
 # Programm zum Zugriff auf entfernte Arbeitsflächen inkl. Erweiterungen
-sudo apt-get purge remmina remmina-plugin-vnc remmina-plugin-rdp
+# 	remmina									# AIP: libfreerdp-plugins-standard libfreerdp1 libssh-4 libvncserver0 remmina-common
+echo ""
+echo ""
+echo -e "\e[38;5;170mUninstall remmina and its extensions\e[0m"
+apt-get purge -y remmina remmina-plugin-vnc remmina-plugin-rdp
+
 
 # Nicht mehr benötigte Pakete entfernen
-sudo apt-get autoremove
+echo ""
+echo ""
+echo -e "\e[38;5;170mCleanup remaining extensions and fragments\e[0m"
+apt-get autoremove -y
 
 
-
+echo -e "\e[38;5;87m...launching the UCS LDAP integration script\e[0m"
+bash /usr/share/univention-wsccubuntu-integration/ucs-ldap-integration.sh
